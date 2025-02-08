5e9d3340d7314a45cbb53b9d27230a1e74524395
himanshu3232 <himanshu2000ranjan@gmail.com>
2025-02-08 11:14:47 +0530

    [Himanshu Ranjan] Add. CompareSortingAlgos init

--- day5/src/main/java/org/capgemini/problem_2/CompareSortingAlgos.java
+++ day5/src/main/java/org/capgemini/problem_2/CompareSortingAlgos.java
@@ -1,6 +1,14 @@
 package org.capgemini.problem_2;
 
+
+//2. Problem Statement: Sorting Large Data Efficiently
+//Objective:
+//Compare sorting algorithms Bubble Sort (O(N²)), Merge Sort (O(N log N)), and Quick Sort (O(N log N)).
+//Approach:
+//Bubble Sort: Repeated swapping (inefficient for large data).
+//Merge Sort: Divide & Conquer approach (stable).
+//Quick Sort: Partition-based approach (fast but unstable).
 public class CompareSortingAlgos {
 
-    //public
+
 }
